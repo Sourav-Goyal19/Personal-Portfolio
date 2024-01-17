52d02eab3b708f9d6a280f7a97ed6a85b049585a
Sourav-Goyal19 <goyalsourav935@gmail.com>
2024-01-17 17:55:11 +0530

    update

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,13 +1,27 @@
-import './Modal.scss'
+import "./Modal.scss";
+import { RxCross2 } from "react-icons/rx";
 
 const Modal = (props) => {
-    return (
-        <div onClick={() => { props.onClose() }} className='ModalContainer'>
-            <div className="modal" onClick={(e) => e.stopPropagation()}>
-                {props.children}
-            </div>
+  return (
+    <div
+      onClick={() => {
+        props.onClose();
+      }}
+      className="ModalContainer"
+    >
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <div className="modalContent">
+          <RxCross2
+            className="cross"
+            onClick={() => {
+              props.onClose();
+            }}
+          />
+          {props.children}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
