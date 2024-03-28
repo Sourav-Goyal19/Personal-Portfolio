3a92fcd12393411c65f9d2c5757d2a4ed735ed5d
Sourav-Goyal19 <goyalsourav935@gmail.com>
2024-03-28 09:13:52 +0530

    Skills Section

--- src/Components/Home/Skills_Section/SkillsSec.jsx
+++ src/Components/Home/Skills_Section/SkillsSec.jsx
@@ -1,8 +1,90 @@
 import React from "react";
 import "./SkillsSec.scss";
-import { LazyLoadImage } from "react-lazy-load-image-component";
+// import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const SkillsSec = (props) => {
+  const frontendSkills = [
+    {
+      name: "Next JS",
+      icon: "/assets/svgs/next.svg",
+    },
+    {
+      name: "React JS",
+      icon: "/assets/svgs/react.svg",
+    },
+    {
+      name: "Tailwind CSS",
+      icon: "/assets/svgs/tailwind.svg",
+    },
+    {
+      name: "Vite",
+      icon: "/assets/svgs/vite.svg",
+    },
+    {
+      name: "Sass",
+      icon: "/assets/svgs/sass.svg",
+    },
+    {
+      name: "Javascript",
+      icon: "/assets/svgs/javascript.svg",
+    },
+    {
+      name: "CSS",
+      icon: "/assets/svgs/css.svg",
+    },
+    {
+      name: "HTML",
+      icon: "/assets/svgs/html.svg",
+    },
+  ];
+  const backendSkills = [
+    {
+      name: "Node JS",
+      icon: "/assets/svgs/node.svg",
+    },
+    {
+      name: "Express JS",
+      icon: "/assets/svgs/express.svg",
+    },
+    {
+      name: "Mongo DB",
+      icon: "/assets/svgs/mongodb.svg",
+    },
+    {
+      name: "Firebase",
+      icon: "/assets/svgs/firebase.svg",
+    },
+  ];
+  const otherSkills = [
+    {
+      name: "Git",
+      icon: "/assets/svgs/git.svg",
+    },
+    {
+      name: "Github",
+      icon: "/assets/svgs/github.svg",
+    },
+    {
+      name: "Postman",
+      icon: "/assets/svgs/postman.svg",
+    },
+    {
+      name: "Python",
+      icon: "/assets/svgs/python.svg",
+    },
+    {
+      name: "Java",
+      icon: "/assets/svgs/java.svg",
+    },
+    {
+      name: "C",
+      icon: "/assets/svgs/c.svg",
+    },
+    {
+      name: "C++",
+      icon: "/assets/svgs/c++.svg",
+    },
+  ];
   return (
     <section
       className="Skills"
@@ -12,179 +94,62 @@ const SkillsSec = (props) => {
         <div data-aos="fade-right" className="Section-Heading">
           <h2 className={`heading skillsHeading`}>Skills</h2>
         </div>
-        <div className="skills-container">
-          <div data-aos="zoom-in" className={`skills-image-container`}>
-            <LazyLoadImage
-              src="/assets/skills/ideogram(3).png"
-              alt="Skills Section Background"
-            />
-          </div>
-          <div className="skills-content">
-            <div className="main-skills-container">
-              <div data-aos="fade-right" className={`main-skills-title`}>
-                <span className="openTag">{`<`}</span>
-                <span className="skills-title">Main</span>
-                <span className="bar">{`/`}</span>
-                <span className="closeTag">{`>`}</span>
-                <span className="arrow">{` =>`}</span>
-              </div>
-              <div
-                data-aos="fade-right"
-                data-aos-delay="500"
-                className={`main-skills-content`}
-              >
-                <div className="slider2">
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/React-icon.png"
-                      alt="React Js"
-                    />
-                    <div className="skill-title">React Js</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/vitejs-icon.png"
-                      alt="Vite Js"
-                    />
-                    <div className="skill-title">Vite Js</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/tailwindCSS-icon.png"
-                      alt="Tailwind CSS"
-                    />
-                    <div className="skill-title">Tailwind CSS</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/js2-icon.png"
-                      alt="JavaScript"
-                    />
-                    <div className="skill-title">JavaScript</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/html-icon.png"
-                      alt="HTML"
-                    />
-                    <div className="skill-title">HTML</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/CSS-icon.png"
-                      alt="CSS"
-                    />
-                    <div className="skill-title">CSS</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/firebase-icon.png"
-                      alt="Firebase"
-                    />
-                    <div className="skill-title">Firebase</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/sass-icon.png"
-                      alt="SASS"
-                    />
-                    <div className="skill-title">SASS</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/React-icon.png"
-                      alt="React Js"
-                    />
-                    <div className="skill-title">React Js</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/vitejs-icon.png"
-                      alt="Vite Js"
-                    />
-                    <div className="skill-title">Vite Js</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/tailwindCSS-icon.png"
-                      alt="Tailwind CSS"
-                    />
-                    <div className="skill-title">Tailwind CSS</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/js2-icon.png"
-                      alt="JavaScript"
-                    />
-                    <div className="skill-title">JavaScript</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/html-icon.png"
-                      alt="HTML"
-                    />
-                    <div className="skill-title">HTML</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/CSS-icon.png"
-                      alt="CSS"
-                    />
-                    <div className="skill-title">CSS</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/firebase-icon.png"
-                      alt="Firebase"
-                    />
-                    <div className="skill-title">Firebase</div>
-                  </div>
-                  <div className="skill-content">
-                    <LazyLoadImage
-                      src="/assets/skills/sass-icon.png"
-                      alt="SASS"
-                    />
-                    <div className="skill-title">SASS</div>
-                  </div>
+        <div className="flex justify-center items-center gap-8 lg:gap-16 flex-wrap mt-10">
+          <div
+            data-aos="fade-right"
+            className="border-2 border-[#FF014F] rounded-lg p-5 w-96 h-72"
+          >
+            <h3 className="text-3xl text-center text-[#c9c8c8] pb-6">
+              Frontend
+            </h3>
+            <div className="flex gap-3 flex-wrap justify-center">
+              {frontendSkills.map((skill, index) => (
+                <div
+                  key={index}
+                  className="skill flex gap-2 items-center border border-[#c9c8c8] p-2 rounded-lg"
+                >
+                  <img src={skill.icon} alt={skill.name} className="h-5 w-5" />
+                  <p className="text-[#878e99]">{skill.name}</p>
                 </div>
-              </div>
+              ))}
             </div>
-            <div className="familiar-skills-container">
-              <div data-aos="fade-right" className={`familiar-skills-title`}>
-                <span className="openTag">{`<`}</span>
-                <span className="skills-title">Familiar</span>
-                <span className="bar">{`/`}</span>
-                <span className="closeTag">{`>`}</span>
-                <span className="arrow">{` =>`}</span>
-              </div>
-              <div
-                data-aos="fade-right"
-                data-aos-delay="500"
-                className={`familiar-skills-content`}
-              >
-                <div className="skill-content">
-                  <LazyLoadImage
-                    src="/assets/skills/python-icon.png"
-                    alt="Python"
-                  />
-                  <div className="skill-title">Python</div>
+          </div>
+          <div
+            data-aos="fade-right"
+            data-aos-delay="300"
+            className="border-2 border-[#FF014F] rounded-lg p-5 w-96 h-72"
+          >
+            <h3 className="text-3xl text-center text-[#c9c8c8] pb-6">
+              Backend
+            </h3>
+            <div className="flex gap-3 flex-wrap justify-center">
+              {backendSkills.map((skill, index) => (
+                <div
+                  key={index}
+                  className="skill flex gap-2 items-center border border-[#c9c8c8] p-2 rounded-lg"
+                >
+                  <img src={skill.icon} alt={skill.name} className="h-5 w-5" />
+                  <p className="text-[#878e99]">{skill.name}</p>
                 </div>
-                <div className="skill-content">
-                  <LazyLoadImage
-                    src="/assets/skills/java-icon.png"
-                    alt="Java"
-                  />
-                  <div className="skill-title">Java</div>
+              ))}
+            </div>
+          </div>
+          <div
+            data-aos="fade-right"
+            data-aos-delay="600"
+            className="border-2 border-[#FF014F] rounded-lg p-5 w-96 h-72"
+          >
+            <h3 className="text-3xl text-center text-[#c9c8c8] pb-6">Other</h3>
+            <div className="flex gap-3 flex-wrap justify-center">
+              {otherSkills.map((skill, index) => (
+                <div
+                  key={index}
+                  className="skill flex gap-2 items-center border border-[#c9c8c8] p-2 rounded-lg"
+                >
+                  <img src={skill.icon} alt={skill.name} className="h-5 w-5" />
+                  <p className="text-[#878e99]">{skill.name}</p>
                 </div>
-                <div className="skill-content">
-                  <LazyLoadImage src="/assets/skills/c-icon.png" alt="C" />
-                  <div className="skill-title">C</div>
-                </div>
-                <div className="skill-content">
-                  <LazyLoadImage src="/assets/skills/c++-icon.png" alt="C++" />
-                  <div className="skill-title">C++</div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
